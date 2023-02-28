b20395917b7c51feb7ac931cd29fd5a034161b64
Sunaa <sunaa@sunaarisu.de>
2023-02-28 15:50:09 +0100

    started nav

--- src/components/generally/nav.vue
+++ src/components/generally/nav.vue
@@ -4,6 +4,7 @@
 
 <template>
     <nav>
+        <div id="burger"></div>
         <div id="logo"></div>
         <div id="searchbar">
             <form action="">
@@ -13,3 +14,34 @@
         <div id="account"></div>
     </nav>
 </template>
+
+<style scoped>
+    nav{
+        display: flex;
+        align-content: center;
+        height: 50px;
+    }
+    #burger{
+        height: 100%;
+        width: 50px;
+    }
+    #logo{
+        height: 100%;
+        width: 50px;
+    }
+    #searchbar{
+        height: 100%;
+        width: 300px;
+    }
+    #searchbar form{
+        height: 100%;
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    #account{
+        height: 100%;
+        width: 50px;
+    }
+</style>
