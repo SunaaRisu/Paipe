1497d56257bf561eaa81b0792cb54a9837f143a4
Sunaa <sunaa@sunaarisu.de>
2023-03-03 11:25:56 +0100

    nav work

--- src/components/generally/sideMenu.vue
+++ src/components/generally/sideMenu.vue
@@ -9,6 +9,7 @@
                 <svg width="30" height="30" viewBox="0 0 95 100" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M17.7986 34.64V98H38.6528V71.12H56.3472V98H77.2014V34.64M2 51.28L47.5 2L93 51.28" stroke="var(--color-text)" stroke-width="6" stroke-linejoin="bevel"/>
                 </svg>
+                <span>Home</span>
             </div>
             <div id="libaryBtnLogo" class="sideMenuBtn">
                 <svg width="30" height="30" viewBox="0 0 54 76" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -34,12 +35,25 @@
         align-items: center;
         justify-content: space-between;
     }
+    nav:hover{
+        width: 250px;
+        align-items: flex-start;
+    }
+    nav:hover .sideMenuBtn span{
+        display: flex;
+    }
+    nav:hover .sideMenuBtn{
+        width: 250px;
+    }
     .sideMenuBtn{
         height: 70px;
         width: 70px;
         display: flex;
         justify-content: center;
         align-items: center;
+    }
+    .sideMenuBtn span{
+        display: none;
     }
     #homeBtnLogo{
         margin-top: 30px;       
@@ -52,7 +66,10 @@
         stroke-width: 8px;
     }
     #termAndPrivacy{
-        margin-bottom: 30px;        
+        margin-bottom: 30px;    
+        width: 70px;
+        display: flex;
+        justify-content: center;    
     }
     #termAndPrivacy a{
         color: var(--color-text);
